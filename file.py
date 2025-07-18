19c9b29365b0aa0640a02063044fd13b171fc914
vishalshrma6168 <garg2003vishal@gmail.com>
2025-07-18 11:02:58 +0530

    all

---
+++ file.py
@@ -0,0 +1,52 @@
+# file=open("sample.txt","r")
+# text=file.read()
+# file.close()
+# print(text)
+
+# file=open("sample.txt","w")
+# file.write("from hp")
+# file.close()
+# print(file)
+
+# f=open("sample.txt","r")
+# txt=f.read()
+# f.close()
+# print(txt)
+
+
+
+
+# f1=open("sample.txt","w")
+# txt=f1.write("hello vishal")
+# f1.close()
+# print(txt)
+
+
+# f=open("sample.txt","a")
+# f.write("how are you")
+# f.close()
+# print(f)
+
+
+# import os
+# os.remove("sample.txt")
+
+# def hello(a,b):
+#   sum=a+b
+#   return sum
+
+# add_value=hello(2,4)
+# print(add_value)
+
+
+# with open("sample.txt","w") as f:
+#   f.write("hello")
+#   f.close()
+#   print(f)
+
+
+firstname="vishal"
+lastname="sharma"
+address="hamirpur"
+# print(f"my  firstname is{firstname}and lastname is{lastname} and my address is{address}")
+print("my firstname is ",firstname,"my lastaname is ",lastname,"my address is ",address)
